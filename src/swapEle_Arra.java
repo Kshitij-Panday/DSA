ae56c2b92322b5e096e3e87c130c28e0fcb96db8
Kshitij-Panday <kshitijpanday06@gmail.com>
2026-03-15 14:53:06 +0530

    arrays

--- src/swapEle_Arra.java
+++ src/swapEle_Arra.java
@@ -41,7 +41,7 @@ public class swapEle_Arra {
             end--;
         }
     }
-
+//swamp fucntion
     static void swap(int[] arr, int index1, int index2) {
         int temp = arr[index1];
         arr[index1] = arr[index2];
