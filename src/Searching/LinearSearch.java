e92f3d42fc7b5c21525cdab6a259d86ba7d90bb0
Kshitij-Panday <kshitijpanday06@gmail.com>
2026-03-17 21:50:41 +0530

    changes

--- src/Searching/LinearSearch.java
+++ src/Searching/LinearSearch.java
@@ -2,6 +2,26 @@ package Searching;
 
 public class LinearSearch {
     public static void main(String[] args) {
-
+int [] nums = {12,15,36,85,253,843,3456,747,57,36,8,157,57,4,8,9,10};
+int target = 10;
+int ans = LinearSearching(nums, target);
+        System.out.println(ans);
     }
+
+    static int LinearSearching(int [] arr , int target){
+        if(arr.length == 0){
+            return -1;
+        }
+
+            for (int i = 0; i < arr.length; i++) {
+            int element = arr[i];
+               if(element == target){
+                   return i;
+
+               }
+
+            }
+      return -1;
+    }
+
 }
