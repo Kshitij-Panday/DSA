c9fe77c8f846fa9e5081e24346de794ffc7620f7
Kshitij-Panday <kshitijpanday06@gmail.com>
2026-03-28 21:32:16 +0530

    Ceiling problem

---
+++ src/Searching/Ceiling.java
@@ -0,0 +1,31 @@
+package Searching;
+
+public class Ceiling {   // also fixed class name (capital C)
+
+    public static void main(String[] args) {
+        int[] arr = {2,3,4,5,6,7,7,7,8,9,11,45,67,78,98};
+        int target = 11;
+
+        int ans = ceilingSearching(arr, target);
+        System.out.println(ans);
+    }
+
+
+    static int ceilingSearching(int[] arr, int target) {
+        int start = 0;
+        int end = arr.length - 1;
+
+        while (start <= end) {
+            int mid = start + (end - start) / 2;
+
+            if (target < arr[mid]) {
+                end = mid - 1;
+            } else if (target > arr[mid]) {
+                start = mid + 1;
+            } else {
+                return mid;
+            }
+        }
+        return start;
+    }
+}
