900515aceedb4cc6f0ff1b70c393c1622d7c939e
Kshitij-Panday <kshitijpanday06@gmail.com>
2026-06-04 20:35:45 +0530

    Bubble Sort

--- src/sorting/BubbleSort.java
+++ src/sorting/BubbleSort.java
@@ -1,30 +1,45 @@
 package sorting;
 
 import java.util.Arrays;
-//bubble sort.
+
+// Bubble Sort
 public class BubbleSort {
+
   public static void main(String[] args) {
-    int [] arr = {2,5,8,7,9,3,11,6};
+
+    int[] arr = {2, 5, 8, 7, 9, 3, 11, 6};
+
     bubbleSort(arr);
+
     System.out.println(Arrays.toString(arr));
   }
-  static void bubbleSort(int[] arr){
-    // boolean condition to check if the array is already sorted or not if yes then the loop breaks
+
+  static void bubbleSort(int[] arr) {
+
+    // checks if array is already sorted
     boolean swapped;
+
     for (int i = 0; i < arr.length; i++) {
-swapped = false;
-      //for each step max item will come at last respective index
-      for (int j = 1; j <= arr.length-i-1; j++) {
-        //swap if the item is smaller than the previous item
-        if (arr[j]<arr[j-1]){
+
+      swapped = false;
+
+      // after each pass, largest element moves to the end
+      for (int j = 1; j <= arr.length - i - 1; j++) {
+
+        // swap if current element is smaller
+        if (arr[j] < arr[j - 1]) {
+
           int temp = arr[j];
-          arr[j] = arr[j-1];
-          arr[j-1] = temp;
+          arr[j] = arr[j - 1];
+          arr[j - 1] = temp;
+
           swapped = true;
         }
-if(!swapped){
-  break;
-}
+      }
+
+      // if no swapping happened, array is sorted
+      if (!swapped) {
+        break;
       }
     }
   }
