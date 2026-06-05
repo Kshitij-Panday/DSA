913f60820893fb998f03bc6b570e9c01617f88f4
Kshitij-Panday <kshitijpanday06@gmail.com>
2026-06-05 13:48:58 +0530

    Selection Sort

--- src/sorting/SelectionSort.java
+++ src/sorting/SelectionSort.java
@@ -1,23 +1,48 @@
-//package sorting;
-//
-//public class SelectionSort {
-//  public static void main(String[] args) {
-//
-//  }
-//  static void selectionSort(int[] arr){
-//    for (int i = 0; i < arr.length; i++) {
-//      int last = arr.length-i-1;
-//      int maxIndex = getMaxIndex(arr,0,last);
-//
-//    }
-//  }
-//  private static int getMaxIndex(int[]arr ,int start , int end){
-//    int max = start;
-//    for (int i = start ; i <=end ; i++) {
-//      if (arr[max] < arr[i]){
-//        max= i;
-//      }
-//
-//    }
-//  }
-//}
+package sorting;
+
+import java.util.Arrays;
+
+public class SelectionSort {
+
+  public static void main(String[] args) {
+
+    int[] arr = {5, 3, 4, 1, 2};
+
+    selection(arr);
+
+    System.out.println(Arrays.toString(arr));
+  }
+
+  static void selection(int[] arr) {
+
+    for (int i = 0; i < arr.length; i++) {
+
+      // find max element in remaining array
+      int last = arr.length - i - 1;
+      int maxIndex = getMaxIndex(arr, 0, last);
+
+      // swap max element with correct position
+      swap(arr, maxIndex, last);
+    }
+  }
+
+  static int getMaxIndex(int[] arr, int start, int end) {
+
+    int max = start;
+
+    for (int i = start; i <= end; i++) {
+      if (arr[max] < arr[i]) {
+        max = i;
+      }
+    }
+
+    return max;
+  }
+
+  static void swap(int[] arr, int first, int second) {
+
+    int temp = arr[first];
+    arr[first] = arr[second];
+    arr[second] = temp;
+  }
+}
