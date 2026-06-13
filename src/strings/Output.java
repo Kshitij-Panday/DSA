9bccae2f42b0c194b2c2ed99dda4266b00be2d9e
Kshitij-Panday <kshitijpanday06@gmail.com>
2026-06-13 13:09:20 +0530

    Strings

---
+++ src/strings/Output.java
@@ -0,0 +1,20 @@
+package strings;
+
+import java.util.Arrays;
+
+public class Output {
+  public static void main(String[] args) {
+    System.out.println(56);
+    {
+     @Deprecated
+        Integer num = new Integer(56); // Integer is going to be deprecated
+        System.out.println(num.toString());
+        System.out.println(num);
+    }
+        System.out.println("Kshitij");
+        System.out.println(Arrays.toString(new int[]{2, 3, 4, 5}));
+
+        String name = null;
+        System.out.println(name);
+  }
+}
