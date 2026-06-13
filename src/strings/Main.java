59cd43fe23d565d72150ce3168b5b88b304091eb
Kshitij-Panday <kshitijpanday06@gmail.com>
2026-06-13 12:25:16 +0530

    Strings

---
+++ src/strings/Main.java
@@ -0,0 +1,10 @@
+package strings;
+
+public class Main {
+  public static void main(String[] args) {
+    String a = "kshitij";
+    System.out.println(a);
+    a = "panday";
+    System.out.println(a);
+  }
+}
