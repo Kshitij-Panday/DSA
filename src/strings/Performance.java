b63bcbb883117169510e3d4d221953ad7c0d0725
Kshitij-Panday <kshitijpanday06@gmail.com>
2026-06-13 14:35:55 +0530

    Strings

---
+++ src/strings/Performance.java
@@ -0,0 +1,16 @@
+package strings;
+
+public class Performance {
+  public static void main(String[] args) {
+    String series = "";
+    for (int i = 0; i < 26 ; i++) {
+      char ch = (char)('a'+i);
+//      System.out.println(ch);
+//      System.out.print(ch);
+      series = series + ch;
+//      System.out.println(series);
+
+    }
+    System.out.println(series);
+  }
+}
