5b5f17acb6bbe8f2a5d3fa2fd17a998bc5c36bdf
Kshitij-Panday <kshitijpanday06@gmail.com>
2026-06-26 22:14:16 +0530

    Bitwise questions

---
+++ src/bitwise/countSetBits.java
@@ -0,0 +1,23 @@
+package bitwise;
+
+public class countSetBits {
+  public static void main(String[] args) {
+    int n = 10;
+    System.out.println(Integer.toBinaryString(n));
+
+    System.out.println(count(n));
+  }
+  private static int count(int n){
+    int count = 0;
+    while(n>0){
+      count++;
+      n = n -(n & -n);
+    }
+
+//    while(n >0){
+//      count++;
+//      n = n & (n-1);
+//    }
+    return count;
+  }
+}
