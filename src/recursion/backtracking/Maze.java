bf76a67574d4ada8f1e17f0e23907ff79c1d68b0
Kshitij-Panday <kshitijpanday06@gmail.com>
2026-08-05 19:09:02 +0530

    Maze Questions

---
+++ src/recursion/backtracking/Maze.java
@@ -0,0 +1,33 @@
+package recursion.backtracking;
+
+import java.lang.foreign.GroupLayout;
+
+public class Maze {
+  public static void main(String[] args) {
+    System.out.println(count(3,3));
+   path("",3,3);
+  }
+
+  static int count(int row,int col){
+    if (row == 1 || col == 1){
+      return 1;
+    }
+    int left = count(row-1,col);
+    int right = count(row,col-1);
+
+    return left+right;
+  }
+
+  static void path(String p, int row , int col){
+    if (row == 1 && col == 1){
+      System.out.println(p);
+      return;
+    }
+    if (row > 1){
+      path(p+'D', row-1, col);
+    }
+    if (col > 1){
+      path(p+'R', row, col-1);
+    }
+  }
+}
