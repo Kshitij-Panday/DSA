c9822e88d4abfc34b1d0b767b1f90c4f3c06c6fd
Kshitij-Panday <kshitijpanday06@gmail.com>
2026-08-07 16:32:21 +0530

    Maze Questions

--- src/recursion/backtracking/AllPathsBackTracking.java
+++ src/recursion/backtracking/AllPathsBackTracking.java
@@ -1,5 +1,7 @@
 package recursion.backtracking;
 
+import java.util.Arrays;
+
 public class AllPathsBackTracking {
   public static void main(String[] args) {
     boolean[][] board ={
@@ -8,6 +10,9 @@ public class AllPathsBackTracking {
             {true,true,true}
     };
     AllPathRestrictions("",board, 0,0);
+
+    int [][] path = new int[board.length][board[0].length];
+    AllPathMatrixPrint("",board,0,0,path,1);
   }
   static void AllPathRestrictions(String p, boolean[][] maze, int row, int col) {
     if (row == maze.length - 1 && col == maze[0].length - 1) {
@@ -31,5 +36,36 @@ public class AllPathsBackTracking {
       AllPathRestrictions(p + 'L', maze, row, col-1);
     }
     maze[row][col] = true;
+  }
+
+  static void AllPathMatrixPrint(String p, boolean[][] maze, int row, int col,int [][] path, int step) {
+    if (row == maze.length - 1 && col == maze[0].length - 1) {
+      for (int [] arr: path){
+        path[row][col] = step;
+        System.out.println(Arrays.toString(arr));
+      }
+      System.out.println(p);
+      System.out.println();
+      return;
+    }
+    if (!maze[row][col]) {
+      return;
+    }
+    maze[row][col] = false;
+    path[row][col] = step;
+    if (row < maze.length - 1) {
+      AllPathMatrixPrint(p + 'D', maze, row + 1, col,path,step+1);
+    }
+    if (col < maze[0].length - 1) {
+      AllPathMatrixPrint(p + 'R', maze, row, col + 1,path,step+1);
+    }
+    if (row > 0) {
+      AllPathMatrixPrint(p + 'U', maze, row-1, col,path,step+1);
+    }
+    if (col > 0) {
+      AllPathMatrixPrint(p + 'L', maze, row, col-1,path,step+1);
+    }
+    maze[row][col] = true;
+    path[row][col] = 0;
   }
 }
