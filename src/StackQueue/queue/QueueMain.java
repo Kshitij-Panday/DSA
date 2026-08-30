923bf88741bfecd8da2390955b8af1f61b4360d4
Kshitij-Panday <kshitijpanday06@gmail.com>
2026-08-30 21:38:39 +0530

    Queue

--- src/StackQueue/queue/QueueMain.java
+++ src/StackQueue/queue/QueueMain.java
@@ -2,7 +2,8 @@ package StackQueue.queue;
 
 public class QueueMain {
   public static void main(String[] args) throws Exception {
-    CustomQueue queue = new CustomQueue();
+//    CustomQueue queue = new CustomQueue();
+      CircularQueue queue = new CircularQueue();
     queue.insert(10);
     queue.insert(20);
     queue.insert(30);
@@ -10,6 +11,7 @@ public class QueueMain {
     queue.insert(50);
     queue.display();
     System.out.println("Removed "+queue.remove());
+    queue.insert(100);
     queue.display();
   }
 }
