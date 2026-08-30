d0aa20eb87aec06a0bd05462c50d4681fe970e39
Kshitij-Panday <kshitijpanday06@gmail.com>
2026-08-30 18:45:04 +0530

    Queue

---
+++ src/StackQueue/queue/CustomQueue.java
@@ -0,0 +1,67 @@
+package StackQueue.queue;
+
+import java.util.Queue;
+
+public class CustomQueue {
+  protected int[] data;
+  private static final int DEFAULT_SIZE = 10;
+
+  int end = 0;
+
+
+
+  public CustomQueue(int size){
+    this.data=new int [size];
+  }
+  public CustomQueue(){
+    this(DEFAULT_SIZE);
+  }
+
+  public boolean isFull(){
+    return end == data.length; //end is at the last place; if you do end++ it will go out of bounds
+  }
+
+  public boolean isEmpty(){
+    return end == 0;
+  }
+
+  public boolean insert(int item){
+    if(isFull()){
+      return false;
+    }
+    data[end++] = item;
+    return true;
+  }
+
+  public int remove() throws Exception{
+    if (isEmpty()){
+      throw new Exception("Queue is Empty");
+
+    }
+    int removed = data[0];
+    for (int i =1; i<end;i++){
+      data[i-1] = data[i];
+    }
+    end--;
+    return removed;
+
+  }
+
+  public int front() throws Exception{
+    if (isEmpty()){
+      throw new Exception("Queue is Empty");
+    }
+    return data[0];
+  }
+
+  public void display(){
+    for (int i = 0; i < end; i++) {
+      System.out.print(data[i]+"|");
+    }
+    System.out.println("END");
+  }
+
+
+
+
+}
---
+++ src/StackQueue/queue/QueueMain.java
@@ -0,0 +1,15 @@
+package StackQueue.queue;
+
+public class QueueMain {
+  public static void main(String[] args) throws Exception {
+    CustomQueue queue = new CustomQueue();
+    queue.insert(10);
+    queue.insert(20);
+    queue.insert(30);
+    queue.insert(40);
+    queue.insert(50);
+    queue.display();
+    System.out.println("Removed "+queue.remove());
+    queue.display();
+  }
+}
